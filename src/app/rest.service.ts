a1a244d6dcd6ed7f3e4135167c49f5499c99f5fe
hanif <dynoboyz@gmail.com>
2019-10-10 22:56:04 +0800

    Add rest service file Remove bootstrap css

---
+++ src/app/rest.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const endpoint = 'https://api.github.com/search/';
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClient) { }
+
+  private extractData(res: Response) {
+    const body = res;
+    return body || { };
+  }
+
+  getGitHubs(currentPage, perPage, search): Observable<any> {
+    let params = new HttpParams();
+    params = params.append('page', currentPage);
+    params = params.append('per_page', perPage);
+    if (search) {
+      params = params.append('q', search);
+    }
+    return this.http.get(endpoint + 'repositories', { params: params }).pipe(
+      map(this.extractData));
+  }
+}
