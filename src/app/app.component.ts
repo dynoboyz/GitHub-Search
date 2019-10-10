551cbdd4f5780bf091fcd53f03b9f8c321e79b0b
hanif <dynoboyz@gmail.com>
2019-10-10 22:17:56 +0800

    Make listing dynamic using sample data Modify search icon to middle with input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'GitHub-Search';
 
+  config = {
+    itemsPerPage: 10,
+    currentPage: 1,
+    totalItems: 0
+  };
+  collection = {
+    status: '',
+    message: '',
+    total_count: 100,
+    items: []
+  };
+  error: any;
+  search = 'fly';
+
+  constructor() {
+    this.getGithubs({itemsPerPage: 10, currentPage: 1});
+  }
+
   pageChanged(event) {
+    this.config.currentPage = event;
+  }
+
+  getGithubs(config: any) {
+    // Create dummy data
+    for (let i = 0; i < this.collection.total_count; i++) {
+      this.collection.items.push({
+          html_url: '',
+          full_name: 'facebook/react ' + i,
+          description: 'A declarative, efficient and flexible Javascript library for building user interfaces.',
+          updated_at: 'Tue Jul 03 2018',
+          language: 'Javascript',
+          stargazers_count: 105262
+      });
+    }
   }
 }
